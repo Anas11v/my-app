80d1715aa4bff6c184690e8b97cfce9a3e4805c0
Anas Alhotairshi <anas.ghazi.1424@gmail.com>
2026-01-19 01:52:23 +0300

    last push of the day

--- shit.js
+++ shit.js
@@ -2,23 +2,43 @@ const express = require('express');
 const si = require('systeminformation');
 const app = express();
 
-const verifyKey = (req, res, next) => {
-  const key = req.headers['x-server-key'];
-  if (key !== process.env.SERVER_SECRET) return res.status(403).send('Forbidden');
-  next();
-};
+//const verifyKey = (req, res, next) => {
+//  const key = req.headers['x-server-key'];
+//  if (key !== process.env.SERVER_SECRET) return res.status(403).send('Forbidden');
+//  next();
+//};
 
 
 
-app.get('/system/static', verifyKey, async (req,res)=> {
-    
-    const cpu = await si.cpu();
-    const os = await si.osInfo();
-    const mem = await si.memInfo();
-    res.json({ cpu, os, mem })
-    });
+// ... inside app.get('/system/stream') ...
 
-app.get('/system/stream', verifyKey, async (req,res) => {
+    const intervalId = setInterval(async () => {
+        try {
+            // 1. Fetch the raw data
+            const [load, temp, mem] = await Promise.all([
+                si.currentLoad(),
+                si.cpuTemperature(),
+                si.mem()
+            ]);
+
+            // 2. Create a CLEAN object (The "ViewModel")
+            const cleanData = {
+                cpu: Math.round(load.currentLoad),      // 23
+                temp: Math.round(temp.main),            // 25
+                ram: Math.round(mem.active / 1024 / 1024 / 1024), // GB used
+                timestamp: Date.now()
+            };
+
+            // 3. Send only the clean string
+            res.write(`data: ${JSON.stringify(cleanData)}\n\n`);
+
+        } catch (err) {
+            console.error(err);
+            // Don't crash the stream on one bad read
+        }
+    }, 2000);
+
+app.get('/system/stream', async (req,res) => {
 
     res.writeHead(200, {
     'Content-Type': 'text/event-stream',
@@ -29,11 +49,24 @@ app.get('/system/stream', verifyKey, async (req,res) => {
         try {
         const load = await si.currentLoad();
         const temp = await si.cpuTemperature();
+        const dataObject = { load, temp };
+        const jsonString = JSON.stringify(dataObject);
+        const sseMessage = 'data: ' + jsonString + '\n\n'
+        res.write(sseMessage)
+        }catch (error) {
+            // FIX 2: Do not use res.status() here, headers are already sent. 
+            // Just end the stream or send a data error.
+            res.end(); 
+            clearInterval(intervalId);
         }
     }, 2000 )
-    
+
+    req.on('close', () => {
+    clearInterval(intervalId);
+    console.log('Client disconnected, stopping loop.');
+    });
 });
 
 app.listen(3000,()=>{
-    console.log('app lising')
+    console.log('app listing')
 });
