6f20b73d3542cea512f66168d31336bd597719f9
Anas Alhotairshi <anas.ghazi.1424@gmail.com>
2026-01-18 20:56:22 +0300

    added a few things
    added an interval loop adjusted the static get only stream get left top work on

--- shit.js
+++ shit.js
@@ -8,18 +8,30 @@ const verifyKey = (req, res, next) => {
   next();
 };
 
+
+
 app.get('/system/static', verifyKey, async (req,res)=> {
     
+    const cpu = await si.cpu();
+    const os = await si.osInfo();
+    const mem = await si.memInfo();
+    res.json({ cpu, os, mem })
+    });
+
+app.get('/system/stream', verifyKey, async (req,res) => {
+
     res.writeHead(200, {
     'Content-Type': 'text/event-stream',
     'Cache-Control': 'no-cache',
     'Connection': 'keep-alive',
-      }); 
-    data =await si.getStaticData()
     });
-
-app.get('/system/stream', verifyKey, async (req,res) => {
-    res.send('hello world');
+    setInterval( async () => {
+        try {
+        const load = await si.currentLoad();
+        const temp = await si.cpuTemperature();
+        }
+    }, 2000 )
+    
 });
 
 app.listen(3000,()=>{
